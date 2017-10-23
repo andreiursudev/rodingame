601e93679238fef318e03a3c410d1761daa3a8e4
Andrei Ursu <ursuandrei42@gmail.com>
2017-10-23 23:01:37 +0300

    add websocket annotations

--- src/main/java/ro/rodin/Game.java
+++ src/main/java/ro/rodin/Game.java
@@ -1,7 +1,12 @@
 package ro.rodin;
 
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/game")
 public class Game {
 
+    @OnMessage
     public String play(){
         return "You won!";
     }
