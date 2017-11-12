940daccce73f587c4ef0d972af6929d552248fff
Andrei Ursu <ursuandrei42@gmail.com>
2017-11-12 19:36:44 +0200

    #3 player cand send a message to draw a door card

--- src/main/java/ro/rodin/Game.java
+++ src/main/java/ro/rodin/Game.java
@@ -3,11 +3,16 @@ package ro.rodin;
 import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/game")
+@ServerEndpoint("/player")
 public class Game {
 
     @OnMessage
     public String play(String playerName){
+            switch (Message.valueOf(playerName)){
+                case DRAW_DOOR_CARD:
+                    break;
+            }
+
         return playerName + ", you won!";
     }
 
---
+++ src/main/java/ro/rodin/Message.java
@@ -0,0 +1,5 @@
+package ro.rodin;
+
+public enum Message {
+    DRAW_DOOR_CARD
+}
